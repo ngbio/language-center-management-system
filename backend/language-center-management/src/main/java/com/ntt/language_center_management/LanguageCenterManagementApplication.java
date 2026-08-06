217df073d1ad5f53a8483e44fe8281dcba109fcd
Nguyễn Thuận <thuantokutou@gmail.com>
2026-08-06 20:06:02 +0700

    chore: initialize Spring Boot backend project

---
+++ backend/language-center-management/src/main/java/com/ntt/language_center_management/LanguageCenterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ntt.language_center_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageCenterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageCenterManagementApplication.class, args);
+	}
+
+}
---
+++ backend/language-center-management/src/test/java/com/ntt/language_center_management/LanguageCenterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntt.language_center_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageCenterManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
