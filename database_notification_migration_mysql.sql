626f383aab6fede12436908aa95d5ef80b83b447
Nguyễn Thuận <thuantokutou@gmail.com>
2026-09-12 14:53:44 +0700

    feat: remove deprecated notification migration script

--- database_notification_migration_mysql.sql
+++
@@ -1,6 +0,0 @@
--- Run once on an existing database created before student notifications were implemented.
-ALTER TABLE notification
-    ADD COLUMN dedup_key VARCHAR(160) NULL AFTER read_at;
-
-CREATE UNIQUE INDEX uq_notification_dedup_key
-    ON notification(dedup_key);
